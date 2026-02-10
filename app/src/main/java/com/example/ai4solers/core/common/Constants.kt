7eaf0623a82e49ff2f9690db7c610fa5aee5b210
JismeanDev04 <triet.hm.gremsysoutions@gmail.com>
2026-02-10 17:20:16 +0700

    hide api key :)

--- app/src/main/java/com/example/ai4solers/core/common/Constants.kt
+++ app/src/main/java/com/example/ai4solers/core/common/Constants.kt
@@ -10,8 +10,6 @@ object Constants {
     //API key
     //xem trong local.properties, nham de tranh leak key -> ton token
     //test
-    const val CLIP_DROP_API_KEY = "7e5a67e6247fbde52e35e005e9c2077c5fa4d29339808d41778261f1a361351c4b3d942a19a27ac84f13f5740c38e481"
-    const val REMOVE_BG_API_KEY = "E1pQmhg5sYort4tV7bFQYrhK"
 
     //base url
     const val CLIP_DROP_URL = "https://clipdrop-api.co/"
